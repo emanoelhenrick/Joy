7cb422cc14b9de6c3c055db9d141e5cbd437bbc4
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2025-01-26 13:43:31 -0300

    feat: add info and restyle the trending banner

--- src/pages/home/components/Trending.tsx
+++ src/pages/home/components/Trending.tsx
@@ -5,6 +5,9 @@ import { MovieDb, TrendingResponse } from "moviedb-promise";
 import { useCallback, useEffect, useState } from "react";
 import { format } from "date-fns";
 import electronApi from "@/config/electronApi";
+import { PiInfoFill } from "react-icons/pi";
+import { AlertDialog, AlertDialogContent, AlertDialogDescription, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
 
 export function Trending() {
   const [data, setData] = useState<TrendingResponse['results']>()
@@ -13,7 +16,7 @@ export function Trending() {
       const meta = await electronApi.getMetadata()
       if (!meta.tmdbKey) return
       const moviedb = new MovieDb(meta.tmdbKey)
-      const res = await moviedb.trending({ media_type: 'all', time_window: 'week', language: 'pt-BR'})
+      const res = await moviedb.trending({ media_type: 'all', time_window: 'day', language: 'pt-BR'})
       setData(res.results)
     }
 
@@ -21,21 +24,34 @@ export function Trending() {
       fetchTrending()
     }, [])
 
+    console.log(data);
+    
+
     const renderItem = useCallback((info: any) => {
       const releaseDate = format(info.release_date || info.first_air_date, "u")
       return (
         <CarouselItem> 
           <div className="flex items-center h-full justify-center rounded-2xl overflow-hidden">
-            <div className="grid grid-cols-[1fr_1fr] 2xl:grid-cols-[3fr_2fr] h-full relative">
-              <div className="p-12 flex flex-col justify-between gap-2">
+            <div className="flex h-full w-full relative">
+              
+              <div className="p-16 flex flex-col justify-end gap-2 z-20 mt-0 2xl:mt-8">
                 <div className="flex flex-col gap-3 z-10">
-                  <div className="flex items-center gap-2">
-                    <span className="w-fit text-muted-foreground text-lg 2xl:text-xl">
+                  <div className="flex items-end gap-2">
+                    <span className="w-fit text-muted-foreground text-lg 2xl:text-xl leading-none">
                       Trending
-                      <span className="ml-2 text-xs opacity-70">by TMDB</span>
                     </span>
+
+                    <HoverCard openDelay={400}>
+                      <HoverCardTrigger>
+                      <span className="text-xs opacity-50 hover:opacity-40 cursor-pointer">by TMDB</span>
+                      </HoverCardTrigger>
+                      <HoverCardContent>
+                        <span className="text-sm">
+                        TMDB is an external service that provides data about films and series and has no relation to your private playlist.
+                        </span>
+                      </HoverCardContent>
+                    </HoverCard>
                   </div>
-                
                   <h1 className="text-5xl 2xl:text-6xl font-bold">{info.title || info.name}</h1>
                   <div className="flex items-center gap-2">
                     
@@ -45,19 +61,18 @@ export function Trending() {
                     <span className="text-muted-foreground">{releaseDate}</span>
                   </div>
                   
-                  <span className="text-sm 2xl:text-lg line-clamp-5 2xl:line-clamp-6 text-muted-foreground max-w-screen-md">{info.overview}</span>
+                  <span className="text-sm 2xl:text-base line-clamp-5 text-muted-foreground max-w-screen-sm">{info.overview}</span>
                 </div>
-                <Button className="w-fit flex gap-2 justify-self-end z-10">
+                <Button className="w-fit flex gap-2 justify-self-end z-10 mt-3">
                   Search for matches
                 </Button>
               </div>
-
-              <div className="z-10 p-2 h-full">
-                <img className="object-cover rounded-xl h-full shadow-lg z-20" src={`https://image.tmdb.org/t/p/w1280${info.backdrop_path}`} />
-              </div>
             </div>
 
-            <img className="absolute w-full z-0 blur-3xl opacity-20 scale-105" src={`https://image.tmdb.org/t/p/w300${info.backdrop_path}`} />
+            <div className="z-10 w-full h-full absolute flex items-center justify-start">
+              <div className="inset-0 w-full h-full bg-gradient-to-l from-transparent to-background/95" />
+            </div>
+            <img className="absolute w-full" src={`https://image.tmdb.org/t/p/original${info.backdrop_path}`} />
           </div>
         </CarouselItem>
       )
